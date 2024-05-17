381770dcfa1ea37a30d2e3209e9dc3a3ddf035da
Yusuke Utsunomiya <uskay@google.com>
2024-05-17 16:21:46 +0900

    Added explainer in README.md + other nit changes

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const fs = require('fs');
 const key = fs.readFileSync(`${__dirname}/cert/key.pem`);
 const cert = fs.readFileSync(`${__dirname}/cert/cert.pem`);
 const https = require('https');
+
+const settings = new Map();
+process.argv.slice(2).forEach((val, index) => {
+  if(val.includes('=') && val.startsWith('--')){
+    const optionKey = val.split('=')[0];
+    const optionValue = val.split('=')[1]
+    settings.set(optionKey, optionValue);
+  }
+});
+
 const getCookie = (req, searchKey) => {
   const cookie = req.headers.cookie;
   let res = null;
@@ -24,17 +34,21 @@ const genServer = (pages) => {
         res.sendFile(`${__dirname}/index.html`);
     });
     if (pages) {
+      const shouldFix = settings.get('--3pcdfix');
       pages.forEach(html => {
         const page = html;
         const path = page.split('.')[0];
         app.get(`/${path}`, (req, res) => {
+          if (shouldFix) {
+            res.cookie('should-fix-3pcd', true);
+          }   
           res.cookie(`${path}-page-html`, page);
           res.sendFile(`${__dirname}/${page}`);
         });
       });
     }
     app.get('/track', (req, res) => {
-      res.cookie('some-tracking-cookie-name', 'some-tracking-cookie-value');
+      res.cookie('some-tracking-cookie-name', 'some-tracking-cookie-value', {sameSite: 'none', secure: true});
       res.send();
     });
     app.get('/pay', (req, res) => {
@@ -62,7 +76,12 @@ const genServer = (pages) => {
       const response = `${getRandomExpression()}|${getRandomExpression()}|${getRandomExpression()}`;
       chatSession.push({position: 'left', text: response});
       // Demo purpose (do not do this in production!). Simply encode the chat log as session data
-      res.cookie('chat-session', btoa(JSON.stringify(chatSession)), {sameSite: 'none', secure: true, /** partitioned: true **/});
+      const shouldFix = settings.get('--3pcdfix');
+      let option = {sameSite: 'none', secure: true};
+      if (shouldFix) {
+        option['partitioned'] = true;
+      }
+      res.cookie('chat-session', btoa(JSON.stringify(chatSession)), option);
       res.send(response);
     });
     app.get('/chat-log', (req, res) => {
@@ -71,7 +90,8 @@ const genServer = (pages) => {
     });
         
     const listener = https.createServer({key: key, cert: cert }, app).listen(443, _ => {
-      console.log('Sever started!')
+      const shouldFix = settings.get('--3pcdfix');
+      console.log(`Sever started! Should fix 3pcd issues: ${shouldFix ? true : false}`);
     });
 };
 genServer(['product.html', 'purchase.html', 'chat.html', 'payments.html']);
